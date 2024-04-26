7dbd518d598fceeab91f26e36d5582b08dd8c066
ISNKYKO <164849442+ISNKYKO@>
2024-04-26 11:48:02 +0000

    Initial Commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "421505b200a34d3f8474334ed9097548.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
